339b53ad079eacfce38552534b0057e7343e180e
Jonas Heinle <jonas.heinle@ipa.fraunhofer.de>
2026-02-23 07:56:01 +0100

    upgrade

---
+++ docs/source_templates/sphinx-book/conf_base.py
@@ -0,0 +1,18 @@
+"""Shared Sphinx baseline for Kataglyphis docs websites."""
+
+SPHINX_EXTENSIONS = [
+    "myst_parser",
+    "sphinx_design",
+]
+
+HTML_THEME = "sphinx_book_theme"
+
+HTML_THEME_OPTIONS = {
+    "repository_url": "https://github.com/Kataglyphis/Kataglyphis-ContainerHub",
+    "use_repository_button": True,
+    "show_navbar_depth": 2,
+    "navigation_with_keys": True,
+}
+
+HTML_STATIC_PATH = ["_static"]
+HTML_CSS_FILES = ["css/custom.css"]

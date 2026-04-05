95d32f1370a4c2b198ebf92f18953231437b7609
Jonas Heinle <jonasheinle@googlemail.com>
2026-04-05 21:39:29 +0200

    fix

--- fix2.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('linux/scripts/03-media/gstreamer/common/setup-gstreamer.sh', 'r') as f:
-    content = f.read()
-
-content = content.replace("      csound csound-utils libcsound64 libcsound64-dev libcsound-dev pd-csound || true\n", "")
-
-with open('linux/scripts/03-media/gstreamer/common/setup-gstreamer.sh', 'w') as f:
-    f.write(content)
